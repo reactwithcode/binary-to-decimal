0ab4c613fe268699e1c7829cb0011debb970ae7e
hari <thohari@atto.id>
2022-01-10 17:35:48 +0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [binary, setBinary] = useState('');
+	const [number, setNumber] = useState('');
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+
+				<input
+					type="number"
+					value={binary}
+					onChange={(e) => setBinary(e.target.value)}
+				/>
+				<p>
+					<code>
+						{binary} {number}
+					</code>
+				</p>
+				<button
+					onClick={() => {
+						setNumber('null');
+					}}
+				>
+					Convert Binary to Decimal
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/binary.js
@@ -0,0 +1,9 @@
+function convert(arr) {
+	// get the array, and flip it
+	let binarryArray = [];
+	let copyArray = [...arr];
+
+    
+
+	// recognise the I's and their decimal values
+}
